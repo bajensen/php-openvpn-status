d24f4ab9a886ed90d7d281858816d9e98fe23dd0
Brendon Jensen <bjensena@byu.edu>
2017-02-14 11:59:31 -0700

    Add auto-refresh interval.

--- bootstrap.php
+++ bootstrap.php
@@ -5,10 +5,12 @@ if (file_exists('hook.php')) {
     require 'hook.php';
 }
 
-$config = include __DIR__ . '/config/config.php';
+$config = require __DIR__ . '/config/config.default.php';
 
-if ($config === null) {
-    die('Configuration file missing.');
+$localConfig = include __DIR__ . '/config/config.local.php';
+
+if ($localConfig) {
+    $config = array_replace_recursive($config, $localConfig);
 }
 
 \OpenVPN\Config::setConfig($config);
--- index.php
+++ index.php
@@ -24,6 +24,11 @@ $updated = $updated->format(DATE_RFC1036);
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script>
+        setTimeout(function() {
+            location.reload();
+        }, <?= \OpenVPN\Config::getValue('reload_freq'); ?>);
+    </script>
 </head>
 <body>
 <div class="container">
